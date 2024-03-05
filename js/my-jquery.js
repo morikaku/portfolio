18f12b693fddfa92f71b1a1ade9ae73f4019b081
morikaku <160197250+morikaku@users.noreply.github.com>
2024-03-05 14:10:51 +0900

    commit in kiba close #13

---
+++ js/my-jquery.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    // Topへ戻るボタン表示切替
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#go-to-top').fadeIn();
+        } else {
+            $('#go-to-top').fadeOut();
+        }
+    });
+
+    // スムーススクロール
+    $('a[href^="#"]').click(function () {
+        let speed = 500;
+        let href = $(this).attr("href");
+        let target = $(href == "#" || href == "" ? 'html' : href);
+        let position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
+
+});
